af4eacecea44d51a8e66df9c840b3f1c23de28f0
doy <ehdus5958@naver.com>
2018-07-09 12:48:08 +0900

    [Team1_pair1]  step1 implemented (#11)
    * Complete function that create and show user
    
    * Completed partially QnA
    
    * step1 implemented

---
+++ src/main/java/codesquad/web/QuestionController.java
@@ -0,0 +1,35 @@
+package codesquad.web;
+
+import codesquad.web.domain.Question;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class QuestionController {
+    private List<Question> questions = new ArrayList<>();
+
+    @PostMapping("/questions")
+    public String create(Question question) {
+        questions.add(question);
+        return "redirect:/";
+    }
+
+    @GetMapping("/")
+    public String list(Model model){
+        model.addAttribute("questions", questions);
+        return "/index";
+    }
+
+    @GetMapping("/questions/{index}")
+    public String show(@PathVariable int index, Model model){
+        model.addAttribute("question", questions.get(index - 1));
+        return "/qna/show";
+    }
+
+}
